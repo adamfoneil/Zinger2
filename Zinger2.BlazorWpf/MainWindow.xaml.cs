d222edaeba9ba8f706ad6f7bfaea777ddb5f8d3a
Adam O'Neil <adamosoftware@gmail.com>
2022-07-02 18:58:54 -0400

    hello world blazor example

---
+++ Zinger2.BlazorWpf/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace Zinger2.BlazorWpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
