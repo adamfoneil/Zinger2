6901fd4e0e31ef4362678d5ead48d4b7a422ec23
Adam O'Neil <adamosoftware@gmail.com>
2022-07-03 13:49:23 -0400

    trying to get data grid dropdown to work, but not working

--- Zinger2.Wpf/ConnectionsWindow.xaml.cs
+++ Zinger2.Wpf/ConnectionsWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Data;
 using Zinger2.Service;
 using Zinger2.Service.Models;
 using Zinger2.Service.Static;
@@ -16,21 +17,24 @@ namespace Zinger2
 
         public ConnectionsWindow(ConnectionStore connectionStore)
         {
+            DataContext = this;
+
             _connectionStore = connectionStore;
 
             SavedConnections = new ObservableCollection<Connection>();
-            ConnectionTypes = EnumHelper.ToObservableCollection<ConnectionType>();
+            ConnectionTypes = EnumHelper.ToDictionary<ConnectionType>();
 
-            InitializeComponent();            
+            InitializeComponent();
+            ConnectionTypeDropdown.ItemsSource = ConnectionTypes;            
         }
 
-        public ObservableCollection<Connection> SavedConnections { get; }
-        public ObservableCollection<KeyValuePair<int, string>> ConnectionTypes { get; }
+        public ObservableCollection<Connection> SavedConnections { get; set; }
+        public Dictionary<int, string> ConnectionTypes { get; }
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
             var list = await _connectionStore.GetConnectionsAsync();
-            foreach (var item in list) SavedConnections.Add(item);
+            foreach (var item in list) SavedConnections.Add(item);            
         }
     }
 }
