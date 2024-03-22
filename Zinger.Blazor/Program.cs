4a4cc2581e5cb101772bc5a9f58d9f8633b4b4db
Adam O'Neil <adamosoftware@gmail.com>
2024-03-22 12:52:49 -0400

    basics of home page

--- Zinger.Blazor/Program.cs
+++ Zinger.Blazor/Program.cs
@@ -1,3 +1,4 @@
+using Zinger.Service.Interfaces;
 using Zinger.Blazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
 	.AddInteractiveServerComponents();
+
+//builder.Services.AddSingleton<IConnectionStore>();
 
 var app = builder.Build();
 
