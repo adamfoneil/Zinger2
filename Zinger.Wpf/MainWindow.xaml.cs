488aba57c9d9ab9b853d94bee53f4045bd156dca
Adam O'Neil <adamosoftware@gmail.com>
2024-03-30 21:33:26 -0400

    progress on query behavior

--- Zinger.Wpf/MainWindow.xaml.cs
+++ Zinger.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using ICSharpCode.AvalonEdit;
+using System;
+using System.Data;
+using System.Windows;
+using Zinger.Service.Extensions;
+using Zinger.Service.Models;
 using Zinger.ViewModels;
 
 namespace Zinger;
@@ -8,22 +13,22 @@ namespace Zinger;
 /// </summary>
 public partial class MainWindow : Window
 {
-    private readonly MainWindowViewModel _viewModel;
-    private readonly ConnectionsWindow _connectionsWin;        
+	private readonly MainWindowViewModel _viewModel;
+	private readonly ConnectionsWindow _connectionsWin;    
 
-    public MainWindow(MainWindowViewModel viewModel, ConnectionsWindow connectionsWin)
-    {
-        _viewModel = viewModel;
-        DataContext = viewModel;
-        _connectionsWin = connectionsWin;
+	public MainWindow(MainWindowViewModel viewModel, ConnectionsWindow connectionsWin)
+	{
+		_viewModel = viewModel;
+		DataContext = viewModel;
+		_connectionsWin = connectionsWin;
 
-        InitializeComponent();
-    }
+		InitializeComponent();
+	}
 
-    private void MnuFileConnections(object sender, RoutedEventArgs e)
-    {
-        _connectionsWin.Show();
-    }	
+	private void MnuFileConnections(object sender, RoutedEventArgs e)
+	{
+		_connectionsWin.Show();
+	}	
 
 	private void MnuFileExit(object sender, RoutedEventArgs e)
 	{
@@ -32,11 +37,35 @@ public partial class MainWindow : Window
 
 	private void Window_Closed(object sender, System.EventArgs e)
 	{
-        Application.Current.Shutdown();
+		Application.Current.Shutdown();
 	}
 
 	private async void Window_Loaded(object sender, RoutedEventArgs e)
 	{
-        await _viewModel.InitializeAsync();
+		await _viewModel.InitializeAsync();
 	}
+
+	private async void RunQuery(object sender, RoutedEventArgs e)
+	{
+		try
+		{
+			if (_viewModel.CurrentConnection is null) return;
+
+			var queryProvider = _viewModel.CurrentConnection.GetQueryProvider();
+			var data = await queryProvider.ExecuteAsync(new Query()
+			{
+				CommandType = CommandType.Text,
+				Sql = _viewModel.Sql
+			});
+		}
+		catch (Exception exc)
+		{
+			MessageBox.Show(exc.Message);
+		}
+	}
+
+    private void TextEditor_TextChanged(object sender, EventArgs e)
+    {
+		_viewModel.Sql = (sender as TextEditor)?.Text;
+    }
 }
--- Zinger.Wpf/ViewModels/MainWindowViewModel.cs
+++ Zinger.Wpf/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,39 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using Zinger.Service.Interfaces;
 using Zinger.Service.Models;
 
 namespace Zinger.ViewModels
 {
-	public class MainWindowViewModel(IConnectionStore connectionStore)
+	public class MainWindowViewModel(IConnectionStore connectionStore) : INotifyPropertyChanged
 	{
 		public IConnectionStore ConnectionStore { get; } = connectionStore;
+
+		private Connection? _currentConnection;
+		private string? _sql;
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		public Connection? CurrentConnection 
+		{ 
+			get => _currentConnection; 
+			set
+			{
+				_currentConnection = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentConnection)));
+			}
+		}
+
+		public string? Sql
+		{
+			get => _sql;
+			set
+			{
+				_sql = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Sql)));
+			}
+		}
 
 		public async Task InitializeAsync()
 		{
@@ -15,5 +41,5 @@ namespace Zinger.ViewModels
 		}
 
 		public ObservableCollection<Connection> Connections { get; private set; } = [];
-    }
+	}
 }
